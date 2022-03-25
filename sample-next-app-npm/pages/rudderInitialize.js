00084620bed10bfc6463fdb509d8c57463a4a23e
Moumita <mandal.moumita2013@gmail.com>
2022-03-25 20:51:16 +0530

    ready fn called after load fn

--- sample-next-app-npm/pages/rudderInitialize.js
+++ sample-next-app-npm/pages/rudderInitialize.js
@@ -1,10 +1,11 @@
 export async function rudderInitialize() {
   window.rudderanalytics = await import("rudder-sdk-js");
 
-  window.rudderanalytics.ready(() => {
-    console.log("we are all set!!!");
-  });
   window.rudderanalytics.load("<WRITE-KEY>", "<DATA-PLANE-URL>", {
     integrations: { All: true }, // load call options
+  });
+
+  window.rudderanalytics.ready(() => {
+    console.log("we are all set!!!");
   });
 }
